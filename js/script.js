f20cd35f26bc2576f23cf39aef51cae785c95273
usmonaliazamov <azamovusmonjon97@gmail.com>
2022-05-30 11:44:17 +0500

    tugadi

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// var myName = 'Usmonali';
+// var mySurname = 'Azamav';
+// var year = 1997;
+// var jami = myName + ' ' + mySurname + ' ' + year;
+
+// console.log(jami);
+
+var a = 10;
+var c = Math.pow(5);
+var j = c + a;
+
+console.log(j);
